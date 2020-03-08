12b0bfe6774ead0096d72dc4025d3f8d7b9740ad
vanquang239dn <vanquang239dn@gmail.com>
2020-03-08 18:16:39 +0900

    Commit

--- src/main/java/com/example/demo/EcommerceWebApplication.java
+++ src/main/java/com/example/demo/EcommerceWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MybatisDemoApplication {
+public class EcommerceWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MybatisDemoApplication.class, args);
+		SpringApplication.run(EcommerceWebApplication.class, args);
 	}
 
 }
